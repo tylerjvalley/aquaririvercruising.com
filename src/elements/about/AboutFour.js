792f8638a546489c2125dd4185176b417a8c6b60
tvalleyiterate <tyler@iteratestudio.com>
2022-09-19 14:40:39 -0400

    text changes

--- src/elements/about/AboutFour.js
+++ src/elements/about/AboutFour.js
@@ -22,9 +22,9 @@ const AboutFour = ({image}) => {
                                             <p className="text">- Unpack only once, the rest of the time your hotel will travel with you.</p>
                                             <p className="text">- Scenic sailings give you views unattainable by land.</p>
                                             <p className="text">- Made friends onland? <br/> Bring them on board for dinner or have a drink in the lounge.</p>
-                                            <p className="text">- Create your own itinerary, explore what you want and when you want.</p>
+                                            <p className="text">- With our set sailing schedule, you know where we're going and you plan your exploration around it.</p>
                                             <p className="text">- Our Concierge can support you in planning your activities at each destination.</p>
-                                            <p className="text">- Aquari plans with local events in mind to get you the most out of you European exploration.</p>
+                                            <p className="text">- Aquari plans with local events in mind to get you the most out of you European discovery.</p>
                                         </div>
                                     </li>
                                 </ul>
