88669fbe99e5e46892856e2ed47a07c38490dcdf
tvalleyiterate <tyler@iteratestudio.com>
2022-09-20 17:47:45 -0400

    edit bullet points

--- src/elements/about/AboutFour.js
+++ src/elements/about/AboutFour.js
@@ -16,17 +16,12 @@ const AboutFour = ({image}) => {
                                 <h4 className="title second">You're not a tourist with us, you're a traveler.</h4>
                               
                                 <ul className="feature-list">
-                                    <li>
-                                        <div className="title-wrapper">
-                                            
-                                            <p className="text">- Unpack only once, the rest of the time your hotel will travel with you.</p>
-                                            <p className="text">- Scenic sailings provide views unattainable by land.</p>
-                                            <p className="text">- Made friends onland? <br/> Bring them aboard for dinner or have them join you for a drink in the lounge.</p>
-                                            <p className="text">- With our set sailing schedule, you know where we're going and you plan your exploration around it.</p>
-                                            <p className="text">- Our Concierge can support you in planning your activities at each destination.</p>
-                                            <p className="text">- Aquari plans with local events in mind, so you can get the most out of your European discovery.</p>
-                                        </div>
-                                    </li>
+                                    <li className="text">Unpack only once, the rest of the time your hotel will travel with you.</li>
+                                    <li className="text">Scenic sailings provide views unattainable by land.</li>
+                                    <li className="text">Made friends onland? <br/> Bring them aboard for dinner or have them join you for a drink in the lounge.</li>
+                                    <li className="text">With our set sailing schedule, you know where we're going and you plan your exploration around it.</li>
+                                    <li className="text">Our Concierge can support you in planning your activities at each destination.</li>
+                                    <li className="text">Aquari plans with local events in mind, so you can get the most out of your European discovery.</li>
                                 </ul>
                                 {/*}
                                 <div className="about-btn mt--30">
